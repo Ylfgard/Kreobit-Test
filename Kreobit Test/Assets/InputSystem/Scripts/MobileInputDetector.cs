132d50ee77633a55bd682a6fe0cd7458f14b9194
Ylfgard <sashamos1510@gmail.com>
2022-02-19 00:32:27 +0300

    Андроид билд

--- Kreobit Test/Assets/InputSystem/Scripts/MobileInputDetector.cs
+++ Kreobit Test/Assets/InputSystem/Scripts/MobileInputDetector.cs
@@ -9,19 +9,15 @@ namespace InputSystem
 
         void Update()
         {
-            if(Input.touchCount > 0)
-            {
-                Touch touch = Input.GetTouch(0); 
-                if(touch.phase != TouchPhase.Began) return;
+            Touch touch = Input.GetTouch(0);
 
-                Vector2 inputPos = Camera.main.ScreenToWorldPoint(touch.position);
-                RaycastHit2D ray = Physics2D.Raycast(inputPos, Vector2.zero);
-                Collider2D c2d = ray.collider;
-                if(c2d == null) 
-                    _inputHandler.InputEmpty();
-                else 
-                    _inputHandler.InputObject(c2d.gameObject);
-            }
+            Vector2 inputPos = Camera.main.ScreenToWorldPoint(touch.position);
+            RaycastHit2D ray = Physics2D.Raycast(inputPos, Vector2.zero);
+            Collider2D c2d = ray.collider;
+            if(c2d == null) 
+                _inputHandler.InputEmpty();
+            else 
+                _inputHandler.InputObject(c2d.gameObject);
         }
     }
 }
