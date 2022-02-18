045bc3caf4dfcbb0b72123cf78cce4d89c582cae
Ylfgard <sashamos1510@gmail.com>
2022-02-19 00:55:49 +0300

    Тесты

--- Kreobit Test/Assets/InputSystem/Scripts/MobileInputDetector.cs
+++ Kreobit Test/Assets/InputSystem/Scripts/MobileInputDetector.cs
@@ -7,13 +7,14 @@ namespace InputSystem
         [SerializeField]
         private GameObject test;
         [SerializeField]
+        private GameObject test2;
+        [SerializeField]
         private InputHandler _inputHandler;
 
         void Update()
         {
             if(Input.touchCount > 0)
             {
-                Instantiate(test, transform.position, Quaternion.identity);
                 Touch touch = Input.GetTouch(0); 
                 if(touch.phase != TouchPhase.Began) return;
 
@@ -21,9 +22,15 @@ namespace InputSystem
                 RaycastHit2D ray = Physics2D.Raycast(inputPos, Vector2.zero);
                 Collider2D c2d = ray.collider;
                 if(c2d == null) 
+                {
+                    Instantiate(test, inputPos, Quaternion.identity);
                     _inputHandler.InputEmpty();
+                }
                 else 
+                {
+                    Instantiate(test2, inputPos, Quaternion.identity);
                     _inputHandler.InputObject(c2d.gameObject);
+                }
             }
         }
     }
